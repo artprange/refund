a029d89e921d250ea35c81e937cff21c3dd63ca2
artprange <arthur.prange@gmail.com>
2024-05-16 12:24:13 -0300

    deleting items, and items amount

--- scripts.js
+++ scripts.js
@@ -3,6 +3,7 @@ const form = document.querySelector('form');
 const amount = document.getElementById('amount');
 const expense = document.getElementById('expense');
 const category = document.getElementById('category');
+const itemQuantity = document.querySelector(' aside header p span');
 
 //selects the list element
 const expenseList = document.querySelector('ul');
@@ -71,20 +72,56 @@ function addExpense(newExpense){
         //creates the expense category
         const expenseCategory = document.createElement('span');
         expenseCategory.textContent = newExpense.category_name;
+        
+
+        //adds the amount to the expense info
+        const  expenseAmount = document.createElement('span');
+        expenseAmount.classList.add('expense-amount');
+        expenseAmount.innerHTML = `<small>R$</small>${newExpense.amount.toUpperCase().replace("R$", "")}`
 
         //adds the information to the expenseInfo div
-
         expenseInfo.append(expenseName, expenseCategory);
+
+        //creates the remove icon
+        const removeIcon = document.createElement('img');
+        removeIcon.classList.add('remove-icon');
+        removeIcon.setAttribute('src', '/img/remove.svg');
+        removeIcon.setAttribute('alt', 'Remover despesa');
+        removeIcon.onclick = () => {
+            expenseItem.remove();
+        }
         
 
         //adds the information to the , list element
-        expenseItem.append(expenseIcon, expenseInfo);
+        expenseItem.append(expenseIcon, expenseInfo, expenseAmount, removeIcon);
 
         //adds the item to the list
         expenseList.append(expenseItem);
+
+        //updates the totals
+        updateTotals();
 
     } catch(error){
         alert("Não foi possível atualizar sua lista de despesas. Tente novamente em alguns minutos.")
     }
    
 }
+
+//updating the totals
+
+function  updateTotals(){
+
+
+    try{
+        //recovers all the li from the ul
+        const items= expenseList.children;
+
+        //updates the list quantity
+        itemQuantity.textContent = `${items.length} ${items.length > 1 ? 'itens' : 'item'}`;
+    } catch (error){
+        console.log(error);
+        alert("Não foi possível atualizar os totais. Tente novamente em alguns minutos.");
+
+    }
+}
+
